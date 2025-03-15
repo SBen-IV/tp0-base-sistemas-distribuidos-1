efe53a6731c61906444950de99336d25c8f1adc6
SBen-IV <sinneo@fi.uba.ar>
2025-03-15 20:39:50 -0300

    fix: validar echo sh-compatible; add files to dockerignore to not rebuild client for every change

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -3,7 +3,7 @@
 MSG_SENT="Hola"
 MSG_RESPONSE=$(echo ${MSG_SENT} | nc server 12345)
 
-if [[ "${MSG_RESPONSE}" == "${MSG_SENT}" ]]; then
+if [ "${MSG_RESPONSE}" == "${MSG_SENT}" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
