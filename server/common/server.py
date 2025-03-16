3465d15d8074e50c968f38b7a9b8d4bcf6da5df5
SBen-IV <sinneo@fi.uba.ar>
2025-03-16 18:57:39 -0300

    refactor: added attribute _server_is_running

--- server/common/server.py
+++ server/common/server.py
@@ -9,6 +9,7 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+        self._server_is_running = True
 
         signal.signal(signal.SIGTERM, self.__stop)
 
@@ -28,13 +29,13 @@ class Server:
 
         # TODO: Modify this program to handle signal to graceful shutdown
         # the server
-        while True:
+        while self._server_is_running:
             try:
                 client_sock = self.__accept_new_connection()
                 self.__handle_client_connection(client_sock)
             except OSError:
                 logging.info("Server socket closed")
-                break
+                self._server_is_running = False
 
     def __handle_client_connection(self, client_sock):
         """
