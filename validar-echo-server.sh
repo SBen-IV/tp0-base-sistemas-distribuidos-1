2687f5f92587ec6facf7114009797809d6ea2898
SBen-IV <sinneo@fi.uba.ar>
2025-03-15 21:53:07 -0300

    refactor: removed netcat from docker compose generator; validar-echo-server now uses a docker container; revert Makefile

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 MSG_SENT="Hola"
-MSG_RESPONSE=$(echo ${MSG_SENT} | nc server 12345)
+
+docker build -f ./netcat/Dockerfile -t "nc-client:latest" .
+
+MSG_RESPONSE=$(docker run --rm --network tp0_testing_net -e MSG_SENT=${MSG_SENT} -v ./netcat/nc.sh:/nc.sh nc-client:latest "/nc.sh")
 
 if test "${MSG_RESPONSE}" = "${MSG_SENT}"; then
     echo "action: test_echo_server | result: success"
